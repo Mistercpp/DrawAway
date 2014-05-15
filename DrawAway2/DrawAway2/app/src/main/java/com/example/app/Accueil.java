11cc90cffa3bee34f683c2b7e5096cacc57b7751
heyheygasy <crobivelo@epsi.fr>
2014-05-15 23:34:05 +0200

    page accueil

---
+++ DrawAway2/DrawAway2/app/src/main/java/com/example/app/Accueil.java
@@ -0,0 +1,77 @@
+package com.example.app;
+
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+public class Accueil extends ActionBarActivity {
+
+    GeometryLayer calque1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView texteView = (TextView) findViewById(R.id.custom_font);
+        Typeface font = Typeface.createFromAsset(getAssets(), "comic.ttf");
+        texteView.setTypeface(font);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.accueil, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_accueil, container, false);
+            return rootView;
+        }
+    }
+
+    public void Quitter(View view) {
+        finish();
+
+    }
+
+}
